8157eac65d1d7da43f46571eeb8bb2f65e23291e
KarunyaR-git <karunya.offl98@gmail.com>
2026-08-29 20:16:41 +0530

    chore:Updated the swagger config with env url

--- backend/src/config/swagger.js
+++ backend/src/config/swagger.js
@@ -9,7 +9,7 @@ const swaggerDefinition = {
     },
     servers: [ 
         {
-            url: 'http://localhost:3000'
+            url: process.env.RENDER_EXTERNAL_URL || 'http://localhost:3000'
         } 
     ]
 };
